fe68ff15e001f53e42ea72c14162de3c5b420f6f
jimchen5209 <jimchen5209@gmail.com>
2023-11-12 13:22:52 +0800

    fix: record card not aligned

--- src/components/centerblock/PlaylistView.vue
+++ src/components/centerblock/PlaylistView.vue
@@ -27,22 +27,19 @@ const playlist = computed(() => {
     class="ts-grid has-spaced-large mobile:is-2-columns tablet-desktop:is-3-columns widescreen:is-5-columns"
   >
     <!-- Column -->
-    <div class="column" v-for:="i in playlist?.slice(cardAmount).reverse()">
-      <RecordCard
-        :key="i.name"
-        :streamer="i.streamer"
-        :publishTime="i.publishTime"
-        :duration="i.duration"
-        :src="i.src"
-        :name="i.name"
-        :isLive="'isLive' in i"
-      />
-    </div>
+    <RecordCard
+      v-for="i in playlist?.slice(cardAmount).reverse()"
+      :key="i.name"
+      class="column"
+      :streamer="i.streamer"
+      :publishTime="i.publishTime"
+      :duration="i.duration"
+      :src="i.src"
+      :name="i.name"
+      :isLive="'isLive' in i"
+    />
     <!-- Scroll top button -->
-    <button
-      class="ts-button is-start-icon is-wide is-fluid"
-      @click="$emit('top')"
-    >
+    <button class="ts-button is-start-icon is-wide is-fluid" @click="$emit('top')">
       <span class="ts-icon is-wheelchair-move-icon"></span>Top
     </button>
     <!-- Load more button -->
